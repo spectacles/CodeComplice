24531c9d3143f7f4abd4981240f472289bb68162
Jonas Karsten <j.karsten@mail.de>
2015-03-09 23:20:21 +0100

    v1.1.2

--- CodeComplice.py
+++ CodeComplice.py
@@ -64,7 +64,7 @@ Configuration files (`~/.codeintel/config' or `project_root/.codeintel/config').
 """
 from __future__ import print_function, unicode_literals
 
-VERSION = "1.1.1"
+VERSION = "1.1.2"
 
 import os
 import re
