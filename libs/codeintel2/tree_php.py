28032e5f832b8a9de70505b6f5066d63f659e5bc
Jonas Karsten <j.karsten@mail.de>
2015-03-20 20:06:56 +0100

    improve return type "this"

--- libs/codeintel2/tree_php.py
+++ libs/codeintel2/tree_php.py
@@ -956,9 +956,10 @@ class PHPTreeEvaluator(TreeEvaluator):
                         ##if is function and return is in (this, )
                         ##return initial class
                         if new_hit[0].get("ilk") == "function" and new_hit[0].get("returns") in ("this",):
+                            nconsumed = 2
                             new_hit = hit
-                            if self.trg.form == TRG_FORM_CALLTIP:
-                                #now get the resolve the calltip on returned class directly!
+                            if self.trg.form == TRG_FORM_CALLTIP and len(remaining_tokens) <= 1:
+                                #now get the resolve the calltip on the last hit class directly!
                                 remaining_tokens = [remaining_tokens[-1]]
                                 new_hit, nconsumed = self._hit_from_getattr(remaining_tokens, *hit)
 
