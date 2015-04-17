899b50750f6765d5b30a3cc0eb539d9b9f7032ef
spectacles <mokula@mail.de>
2015-04-17 12:50:55 +0200

    1.1.3

--- CodeComplice.py
+++ CodeComplice.py
@@ -64,7 +64,7 @@ Configuration files (`~/.codeintel/config' or `project_root/.codeintel/config').
 """
 from __future__ import print_function, unicode_literals
 
-VERSION = "1.1.2"
+VERSION = "1.1.3"
 
 import os
 import re
