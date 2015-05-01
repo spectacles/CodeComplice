5f3ae702a26f48179ee023d56dc7aec8b3411f54
Jonas Karsten <j.karsten@mail.de>
2015-05-01 23:59:15 +0200

    Ruby: catch Type Error

--- libs/codeintel2/parser_cix.py
+++ libs/codeintel2/parser_cix.py
@@ -75,7 +75,10 @@ def get_common_attrs(node):
 
 def sort_by_lines(adict):
     intermed = [(adict[k].line_num, adict[k].type, k) for k in list(adict.keys())]
-    intermed.sort()
+    try:
+        intermed.sort()
+    except TypeError:
+        pass
     return intermed
 
 
